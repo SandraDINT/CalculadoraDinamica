3bfae20b3056b562e0c3c269b7edb10674264ab5
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2021-10-20 10:13:22 +0200

    Hecha

---
+++ CalculadoraDinamica/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculadoraDinamica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            int numeroFilas = gridPrincipal.RowDefinitions.Count - 1;
+            int numeroColumnas = gridPrincipal.ColumnDefinitions.Count - 1;
+            int contador = 0;
+            for (int fila = 0; fila < numeroFilas; fila++)
+            {
+                for (int columnas = 0; columnas < numeroColumnas; columnas++)
+                {
+                    Button boton = new Button();
+                    Grid.SetRow(boton, fila);
+                    Grid.SetColumn(boton, columnas);
+                    boton.Margin = new Thickness(5);
+                    boton.Click += Button_Click;
+                    boton.Tag = contador++;
+                    TextBlock numeroBoton = new TextBlock();
+                    numeroBoton.Text = contador.ToString();
+                    Viewbox viewBox = new Viewbox();
+                    viewBox.Child = numeroBoton;
+                    boton.Content = viewBox;
+                    gridPrincipal.Children.Add(boton);
+                }
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
